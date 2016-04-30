b5b19e8ef76536dc09e0b651dae8c35578864fd6
Ismail Mustafa <ismailmustafa@rocketmail.com>
2016-04-30 11:25:51 -0400

    weather mood calculation complete

--- clean.py
+++ clean.py
@@ -6,6 +6,7 @@ import datetime as dt
 import cPickle as pickle
 import os.path
 import math
+import sys
 
 def main():
 	weatherScore()
@@ -42,25 +43,50 @@ def weatherScore():
 		print "weather data not in memory"
 		all_weather = pandas.read_stata("data/clean/weather_all.dta")
 		all_weather_matrix = all_weather.as_matrix()
+
+		# initialize max values
+		max_prcp = max_snow = max_snwd = max_tsun = max_tavg = sys.float_info.min
+
 		for weather_row in all_weather_matrix:
 			date = weather_row[4]
 			city = weather_row[64]
-			prcp = weather_row[14] # precipitation in 10ths of mm
-			snow = weather_row[16] # snowfall in mm
-			snwd = weather_row[15] # depth of snow in mm
-			tmax = weather_row[21] # highest day temperature in 10th of celcius
-			tmin = weather_row[22] # lowest day temperature in 10th of celcius
-			tsun = weather_row[20] # daily total sunshine in minutes
-			acsc = weather_row[70] # average cloudiness sunrise to sunset (percent)
-			all_weather_dict[str(date) + city] = (nanCheck(prcp), nanCheck(snow), nanCheck(snwd), 
-																					  nanCheck(tmax), nanCheck(tmin), nanCheck(tsun), 
-																					  nanCheck(acsc))
+			prcp = nanCheck(weather_row[14]) # precipitation in 10ths of mm
+			snow = nanCheck(weather_row[16]) # snowfall in mm
+			snwd = nanCheck(weather_row[15]) # depth of snow in mm
+			tmax = nanCheck(weather_row[21]) # highest day temperature in 10th of celcius
+			tmin = nanCheck(weather_row[22]) # lowest day temperature in 10th of celcius
+			tsun = nanCheck(weather_row[20]) # daily total sunshine in minutes
+			all_weather_dict[str(date) + city] = (prcp, snow, snwd, 
+																					  tmax, tmin, tsun)
+
+			# calculate max values
+			if not math.isnan(prcp) and prcp > max_prcp:
+				max_prcp = prcp
+			if not math.isnan(snow) and snow > max_snow:
+				max_snow = snow
+			if not math.isnan(snwd) and snwd > max_snwd:
+				max_snwd = snwd
+			if not math.isnan(tsun) and tsun > max_tsun:
+				max_tsun = tsun
+			if not math.isnan(tmax) and not math.isnan(tmin) and (abs(float(tmax) + float(tmin))/2.0) > max_tavg:
+				max_tavg = (abs(float(tmax) + float(tmin))/2.0)
+
+		# set max values
+		all_weather_dict["max_prcp"] = max_prcp
+		all_weather_dict["max_snow"] = max_snow
+		all_weather_dict["max_snwd"] = max_snwd
+		all_weather_dict["max_tsun"] = max_tsun
+		all_weather_dict["max_tavg"] = max_tavg
+
 		pickle.dump(all_weather_dict, open("data/raw/weather_fast_lookup", "wb"))
 		print "saved weather data"
 
 	print "loading asylum data"
-	asylum_clean_full = pandas.read_csv("data/raw/asylum_clean_full_sample.csv")
+	asylum_clean_full = pandas.read_csv("data/raw/asylum_clean_full.csv")
 	print "loaded asylum data"
+
+	lowest_score = sys.float_info.max
+	highest_score = sys.float_info.min
 
 	# Iterate through asylum clean data and calculate weather score
 	all_scores = []
@@ -75,7 +101,7 @@ def weatherScore():
 		city = row[1][-1]
 
 		# Default to 0.5
-		score_calculation = 0.5
+		score_calculation = 0.0
 		dateCount = 0
 		# iterate through all 5 days of weather
 		weather_scores = []
@@ -86,19 +112,43 @@ def weatherScore():
 				key = date + city
 				if key in all_weather_dict:
 					weather_data = all_weather_dict[key]
-					prcp = weather_data[0] # precipitation in 10ths of mm
-					snow = weather_data[1] # snowfall in mm
-					snwd = weather_data[2] # depth of snow in mm
-					tmax = weather_data[3] # highest day temperature in 10th of celcius
-					tmin = weather_data[4] # lowest day temperature in 10th of celcius
-					tsun = weather_data[5] # daily total sunshine in minutes
-					acsc = weather_data[6] # average cloudiness sunrise to sunset (percent)
-					weather_scores.append(applyWeights(weights(weather_data), weather_data))
+					prcp = 1.0 - safe_divide(weather_data[0], all_weather_dict["max_prcp"]) # precipitation in 10ths of mm
+					snow = 1.0 - safe_divide(weather_data[1], all_weather_dict["max_snow"]) # snowfall in mm
+					snwd = 1.0 - safe_divide(weather_data[2], all_weather_dict["max_snwd"]) # depth of snow in mm
+					tmax = weather_data[3] 																						# highest day temperature in 10th of celcius
+					tmin = weather_data[4] 																						# lowest day temperature in 10th of celcius
+					tavg = (float(tmax) + float(tmin)) / 2.0    											# average day temperature in 10th of celcius
+					tscore = temp_score(tavg)
+					tsun = safe_divide(weather_data[5], all_weather_dict["max_tsun"]) # daily total sunshine in minutes
+
+					calculations = prcp, snow, snwd, tscore, tsun
+					weather_scores.append(applyWeights(weights(calculations), calculations))
 		if dateCount != 0:
 			score_calculation = compute_score(weather_scores)
+			if score_calculation > highest_score:
+				highest_score = score_calculation
+			if score_calculation < lowest_score and score_calculation > 0.01 :
+				lowest_score = score_calculation
+
 		all_scores.append(score_calculation)
 	asylum_clean_full['weather_mood'] = all_scores
-	asylum_clean_full.to_csv('data/raw/asylum_clean_full_sample_weathermood.csv', index=False, index_label=False)
+	asylum_clean_full.to_csv('data/raw/asylum_clean_full_weather.csv', index=False, index_label=False)
+
+	print "highest score:", highest_score
+	print "lowest_score:", lowest_score
+
+def temp_score(tavg):
+	# return 1 - (abs(tavg - 136.0) / max_tavg)
+	if tavg <= 0.0 or tavg >= 272:
+		return 0.0
+	else:
+		return 1.0 - (abs(tavg - 136.0) / 136.0)
+
+def safe_divide(val, max_val):
+	if not math.isnan(val):
+		return float(val) / float(max_val)
+	else:
+		return val
 
 # determine weights for weather score
 def weights(weather_data):
@@ -121,10 +171,9 @@ def applyWeights(weights, weather_data):
 	return score
 
 def compute_score(weather_scores):
-	maximum = max(weather_scores)
-	normalized = [float(x)/maximum for x in weather_scores]
-	return sum(normalized) / float(len(normalized))
-
+	if len(weather_scores) == 0:
+		return 0.0
+	return sum(weather_scores) / float(len(weather_scores))
 
 def getCompletionDate(stata_date):
 	date1960Jan1 = dt.datetime(1960,01,01)
